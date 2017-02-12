ad81a9b1fa508c24ce7aa6f5d4592906ef714fca
vetrok <vetrok87@gmail.com>
2017-02-13 00:35:39 +0200

    Controller added

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -9,13 +9,91 @@
 
 namespace Application\Controller;
 
+use Application\Model\Doctrine\Users;
+use Application\Model\Mailchimp\MailchimpAPI;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
+
 class IndexController extends AbstractActionController
 {
+    /**
+     * @var DoctrineORMEntityManager
+     */
+    protected $em;
+
+    /**
+     * @return DoctrineORMEntityManager|array|object
+     */
+    public function getEntityManager()
+    {
+        if (null === $this->em) {
+            $this->em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+        }
+        return $this->em;
+    }
+
+    /**
+     * Showing subscribers from database
+     *
+     * @return array|void
+     */
     public function indexAction()
     {
-        return new ViewModel();
+        $em = $this->getEntityManager();
+        $allUsers = $em->getRepository('\Application\Model\Doctrine\Users')->findAll();
+
+        return new ViewModel(array('users' => $allUsers));
+    }
+
+    /**
+     * Sync data from API with DB and show it to user
+     *
+     * @return null
+     */
+    public function syncAction()
+    {
+        try {
+            $request = $this->getRequest();
+            if (!$request->isPost()) {
+
+                throw new \Exception();
+            }
+
+            //Get all subscribers from API
+            $mailchimpAPIKey = $this->getServiceLocator()->get('Config')['mailChimpAPIKey'];
+            $mailchimp = new MailchimpAPI();
+            $mailchimp->setAPIKey($mailchimpAPIKey);
+            $subscribers = $mailchimp->retrieveAllSubscribersFromAllLists(true);
+
+            //Because the task contains only one customer, delete all records from DB and insert new from API
+            $em = $this->getEntityManager();
+            $connection = $em->getConnection();
+            $platform = $connection->getDatabasePlatform();
+            $connection->executeQuery('SET FOREIGN_KEY_CHECKS = 0;');
+            $truncateSql = $platform->getTruncateTableSQL('users');
+            $connection->executeUpdate($truncateSql);
+            $connection->executeQuery('SET FOREIGN_KEY_CHECKS = 1;');
+
+            //Insert new users
+            if (!empty($subscribers)) {
+                foreach ($subscribers as $subscriber) {
+                    $user = new Users();
+                    $user->setEmail($subscriber['email']);
+                    $user->setFirstName($subscriber['first_name']);
+                    $user->setLastName($subscriber['last_name']);
+                    $em->persist($user);
+                    $em->flush();
+                }
+            }
+        } catch (\Exception $e) {
+            $errorMsg = 'Can\'t synchronize, please try later';
+        } finally {
+
+            //Redirect back to users list
+            return $this->redirect()->toRoute('home', array(
+                'errorMsg' => isset($errorMsg) ? $errorMsg : null
+            ));
+        }
     }
 }
